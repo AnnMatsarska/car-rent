330784e02ae3a2fa2fccc76abf7908c2e53a0046
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-02-16 15:20:34 +0000

    Deploying to gh-pages from @ AnnMatsarska/car-rent@32da4356a63f8f6121fd8f7e9c49626a27299029 🚀

---
+++ static/js/793.b812efce.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcar_rent=self.webpackChunkcar_rent||[]).push([[793],{793:function(e,r,n){n.r(r);var t=n(184);r.default=function(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)("h1",{children:"FavoritePage"})})}}}]);
+//# sourceMappingURL=793.b812efce.chunk.js.map
