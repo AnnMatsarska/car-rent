8f322a0d3326e63654ef4a9a54ebeb8d9db69e5f
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2024-02-16 19:23:25 +0200

    add: car text

--- src/components/CarItem/CarItem.jsx
+++ src/components/CarItem/CarItem.jsx
@@ -1,9 +1,51 @@
-import { Item, Img } from './CarItem.styled';
+import {
+  Item,
+  Img,
+  CarTitle,
+  CarTextTitle,
+  CarTextSpan,
+  CarTextSubTitle,
+  CarTextSubSpan,
+} from './CarItem.styled';
 
 export const CarItem = ({ car }) => {
+  const {
+    img,
+    model,
+    make,
+    year,
+    rentalPrice,
+    address,
+    rentalCompany,
+    type,
+    mileage,
+    functionalities,
+  } = car;
+  const parseAdress = address.split(',');
+  const city = parseAdress[1];
+  const country = parseAdress[2];
   return (
     <Item>
-      <Img src={car.img} alt="Car" />
+      <Img src={img} alt="Car" />
+      <CarTitle>
+        <CarTextTitle>
+          {make} <CarTextSpan>{model}, </CarTextSpan>
+          {year}
+        </CarTextTitle>
+        <CarTextTitle>${rentalPrice}</CarTextTitle>
+      </CarTitle>
+      <CarTextSubTitle>
+        <CarTextSubSpan>{city}</CarTextSubSpan>
+        <CarTextSubSpan>{country}</CarTextSubSpan>
+        <CarTextSubSpan>{rentalCompany}</CarTextSubSpan>
+        <CarTextSubSpan>Premium</CarTextSubSpan>
+      </CarTextSubTitle>
+      <CarTextSubTitle>
+        <CarTextSubSpan>{type}</CarTextSubSpan>
+        <CarTextSubSpan>{model}</CarTextSubSpan>
+        <CarTextSubSpan>{mileage}</CarTextSubSpan>
+        <CarTextSubSpan>{functionalities[0]}</CarTextSubSpan>
+      </CarTextSubTitle>
     </Item>
   );
 };
--- src/components/CarItem/CarItem.styled.js
+++ src/components/CarItem/CarItem.styled.js
@@ -11,4 +11,45 @@ export const Img = styled.img`
   height: 268px;
   object-fit: cover;
   border-radius: 14px;
+  margin-bottom: 14px;
+`;
+
+export const CarTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 8px;
+  color: var(--secondary-color-black);
+`;
+export const CarTextTitle = styled.p`
+  font-weight: 500;
+  line-height: 24px;
+`;
+export const CarTextSpan = styled.span`
+  color: var(--primary-color-blue);
+`;
+
+export const CarTextSubTitle = styled.p`
+  color: var(--secondary-color-gray);
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 1.5;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  &:not(:last-child) {
+    margin-bottom: 4px;
+  }
+`;
+
+export const CarTextSubSpan = styled.span`
+  &:not(:last-child)::after {
+    content: '';
+    vertical-align: middle;
+    display: inline-block;
+    width: 1px;
+    height: 16px;
+    margin: 0 6px;
+    background-color: rgba(18, 20, 23, 0.1);
+  }
 `;
